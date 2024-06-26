fb63aa565184c94cc6383ee9a50ab58409b7bfb5
Fensley <zagalufe@gmail.com>
2024-06-26 13:22:29 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const view = document.getElementById("view");
+const like = document.getElementById("like");
+view.addEventListener("click", () => {
+  like.innerHTML = "More details comming soon ";
+});
